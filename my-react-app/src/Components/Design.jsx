4c3d370f03a27e43f44cdf8504c35680cc40248c
Gouda12345 <gouda8051@gmail.com>
2025-03-06 11:52:50 +0530

    first commit

--- my-react-app/src/Components/Design.jsx
+++ my-react-app/src/Components/Design.jsx
@@ -3,6 +3,7 @@ import "./Design.css";
 import { LuCalendarMinus2 } from "react-icons/lu";
 import { MdOutlineNotifications } from "react-icons/md";
 import { TbUsersGroup } from "react-icons/tb";
+import { AiOutlineCheck } from "react-icons/ai";
 
 const Design = () => {
     return (
@@ -30,13 +31,16 @@ const Design = () => {
                         </div>
                         <div className="line"></div>
                         <div className="description">Description :</div>
-                        <div className="design-description">Design a simple home pages with clean layout and color based on the guidelines..</div>
+                        <div className="design-description">Design a simple home pages with clean<br></br> layout and color based on the guidelines..</div>
                         <div className="team">
                             <div className="teams">Teams :</div>
                             <div className="progress">Progress :</div>
                         </div>
                         <div className="team-icons">
-                            <div className="teams-icons"><a className="img1">jhjj</a></div>
+                            <div className="teams-icons"><img src="https://e7.pngegg.com/pngimages/694/639/png-clipart-computer-icons-user-profile-female-symbol-miscellaneous-purple.png"/></div>
+                            <div className="teams-icons"> <img src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png"/></div>
+                            <div className="teams-icons"> <img src="https://i.pinimg.com/1200x/26/44/4f/26444f66c8cfe2798e892a669e4df8cf.jpg"/></div>
+                            <div className="teams-icons"><img src="https://e7.pngegg.com/pngimages/694/639/png-clipart-computer-icons-user-profile-female-symbol-miscellaneous-purple.png"/></div>
                             <div className="progress-bar"><div className="progress-circle"></div></div>
                             <div className="number">78%</div>
                         </div>
@@ -49,17 +53,85 @@ const Design = () => {
                         <div className="descr">Descr</div>
                         <div className="meet"> Meet pager</div>
                         <div className="teams1">Team</div>
+                        <div className="teams-icons1"><img src="https://e7.pngegg.com/pngimages/694/639/png-clipart-computer-icons-user-profile-female-symbol-miscellaneous-purple.png"/></div>
                         <div className="bottom-line2"></div>
                     </div>
                 </div>
                 <div className="complete">
                     <div className="completed">Completed</div>
-                    <div className="view">View More</div>
+                    <div className="view1">View More</div>
                 </div>
-                <div className="meeting-box"></div>
+                <div className="meeting-box">
+                    <div className="meeting">
+                       <div className="meeting-title">Meeting with Client</div>
+                       <div className="check"><AiOutlineCheck /></div>
+                    </div>
+                    <div className="redesign">Redesign motion graphic.</div>
+                    <div className="line2"></div>
+                    <div className="today">
+                        <div className="day">Today 11:25 PM</div>
+                        <div className="image-icon"><img src="https://i.pinimg.com/1200x/26/44/4f/26444f66c8cfe2798e892a669e4df8cf.jpg"/></div>
+                        <div className="image-icons"><img src="https://e7.pngegg.com/pngimages/694/639/png-clipart-computer-icons-user-profile-female-symbol-miscellaneous-purple.png"/></div>
+                    </div>
+                </div>
+                <div className="meeting-box2">
+                    <div className="task">
+                       <div className="task-title">Task Explore NFT</div>
+                       <div className="check"><AiOutlineCheck /></div>
+                    </div>
+                    <div className="explore">Explore design mobile UI.</div>
+                    <div className="line2"></div>
+                    <div className="todays">
+                        <div className="days">01 July 10:30AM</div>
+                        <div className="image-icon1"><img src="https://i.pinimg.com/1200x/26/44/4f/26444f66c8cfe2798e892a669e4df8cf.jpg"/></div>
+                        <div className="image-icons1"><img src="https://e7.pngegg.com/pngimages/694/639/png-clipart-computer-icons-user-profile-female-symbol-miscellaneous-purple.png"/></div>
+                    </div>
+                </div>
+                <div className="create-box">
+                    <div className="plus">+<a className="create">Create New</a>
+                    </div>
+                </div>
             </div>
             <div className="second-box">
-                ggg
+                <div className="profile2">
+                    <div className="profile-images2">
+                        <img src="https://static.vecteezy.com/system/resources/previews/010/966/743/non_2x/avatar-nerd-man-free-vector.jpg"></img>
+                        <div className="hello2">Hello, <div className="joko2">Joko Husein</div></div>
+                    </div>
+                    <div className="top-icons11"><LuCalendarMinus2 /></div>
+                    <div className="top-icons22"><MdOutlineNotifications /></div>
+                </div>
+                <div className="progress2">
+                    <div className="on-progress2">On Progress<a className="num"> (12)</a></div>
+                    <div className="view2">View More</div>
+                </div>
+                <div className="design2">
+                    <div className="design-box2">
+                        <div className="design-ui2">
+                            <div className="design-title2">Design UI ToDo APP
+                                <div className="fri2">Friday, 08 July 2022</div>
+                            </div>
+                            <div className="user-icon2"><TbUsersGroup /></div>
+                        </div>
+                    </div>
+                    <div className="wee-box2">
+                        <div className="wee2">Wee</div>
+                        <div className="friday2">Friday</div>
+                    </div>
+                </div>
+                <div className="new-box">
+                    <div className="new-line"></div>
+                    <div className="new-title">New Task ToDo</div>
+                    <div className="tbottom-line"></div>
+                    <div className="title-task">Title Task</div>
+                    <div className="add-box">
+                        <div className="add-title">Add Task Name..</div>
+                    </div>
+                </div>
+                
+
+
+                
             </div>
         </div>
     );
